ef7a635c9c210011519cf75f119b0e0b303961db
valts08 <valts.pilans@gmail.com>
2025-03-13 09:49:01 -0400

    fixed bug with removing items

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -32,12 +32,12 @@ const CartItem = ({ onContinueShopping }) => {
     if (item.quantity > 1) {
         dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }))
     } else {
-        dispatch(removeItem(item))
+        dispatch(removeItem(item.name))
     }
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem(item))
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
