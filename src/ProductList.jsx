8a4217f3c89c4ae692264cf55f458e3e7453e389
valts08 <valts.pilans@gmail.com>
2025-03-13 09:44:36 -0400

    fixed issue with rendering the plant list

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -288,7 +288,7 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
-                    {plantsArray.map((category, index) => {
+                    {plantsArray.map((category, index) => (
                         <div key={index}>
                             <h1><div>{category.category}</div></h1>
                             <div className="product-list">
@@ -302,7 +302,7 @@ function ProductList({ onHomeClick }) {
                                 ))}
                             </div>
                         </div>
-                    })}
+                        ))}
 
                 </div>
             ) : (
